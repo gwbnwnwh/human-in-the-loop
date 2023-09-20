65ae55af446a8e533fbff9e96158cf76d11762ea
gwbnwnwh <swish@stu2021.jnu.edu.cn>
2023-09-20 23:24:08 +0800

    upload

---
+++ pykt-toolkit/examples/wandb_predict.py
@@ -0,0 +1,137 @@
+import os
+import argparse
+import json
+import copy
+import torch
+import pandas as pd
+
+from pykt.models import evaluate,evaluate_question,load_model
+from pykt.datasets import init_test_datasets
+
+device = "cpu" if not torch.cuda.is_available() else "cuda"
+os.environ['CUBLAS_WORKSPACE_CONFIG']=':4096:2'
+
+with open("../configs/wandb.json") as fin:
+    wandb_config = json.load(fin)
+
+def main(params):
+    if params['use_wandb'] ==1:
+        import wandb
+        os.environ['WANDB_API_KEY'] = wandb_config["api_key"]
+        wandb.init(project="wandb_predict")
+
+    save_dir, batch_size, fusion_type = params["save_dir"], params["bz"], params["fusion_type"].split(",")
+
+    with open(os.path.join(save_dir, "config.json")) as fin:
+        config = json.load(fin)
+        model_config = copy.deepcopy(config["model_config"])
+        for remove_item in ['use_wandb','learning_rate','add_uuid','l2']:
+            if remove_item in model_config:
+                del model_config[remove_item]    
+        trained_params = config["params"]
+        fold = trained_params["fold"]
+        model_name, dataset_name, emb_type = trained_params["model_name"], trained_params["dataset_name"], trained_params["emb_type"]
+        if model_name in ["saint", "sakt", "atdkt"]:
+            train_config = config["train_config"]
+            seq_len = train_config["seq_len"]
+            model_config["seq_len"] = seq_len   
+
+    with open("../configs/data_config.json") as fin:
+        curconfig = copy.deepcopy(json.load(fin))
+        data_config = curconfig[dataset_name]
+        data_config["dataset_name"] = dataset_name
+        if model_name in ["dkt_forget", "bakt_time"]:
+            data_config["num_rgap"] = config["data_config"]["num_rgap"]
+            data_config["num_sgap"] = config["data_config"]["num_sgap"]
+            data_config["num_pcount"] = config["data_config"]["num_pcount"]
+        elif model_name == "lpkt":
+            data_config["num_at"] = config["data_config"]["num_at"]
+            data_config["num_it"] = config["data_config"]["num_it"]    
+    if model_name not in ["dimkt"]:        
+        test_loader, test_window_loader, test_question_loader, test_question_window_loader = init_test_datasets(data_config, model_name, batch_size)
+    else:
+        diff_level = trained_params["difficult_levels"]
+        test_loader, test_window_loader, test_question_loader, test_question_window_loader = init_test_datasets(data_config, model_name, batch_size, diff_level=diff_level)
+
+    print(f"Start predicting model: {model_name}, embtype: {emb_type}, save_dir: {save_dir}, dataset_name: {dataset_name}")
+    print(f"model_config: {model_config}")
+    print(f"data_config: {data_config}")
+
+    model = load_model(model_name, model_config, data_config, emb_type, save_dir)
+
+    save_test_path = os.path.join(save_dir, model.emb_type+"_test_predictions.txt")
+
+    if model.model_name == "rkt":
+        dpath = data_config["dpath"]
+        dataset_name = dpath.split("/")[-1]
+        tmp_folds = set(data_config["folds"]) - {fold}
+        folds_str = "_" + "_".join([str(_) for _ in tmp_folds])
+        rel = None
+        if dataset_name in ["algebra2005", "bridge2algebra2006"]:
+            fname = "phi_dict" + folds_str + ".pkl"
+            rel = pd.read_pickle(os.path.join(dpath, fname))
+        else:
+            fname = "phi_array" + folds_str + ".pkl" 
+            rel = pd.read_pickle(os.path.join(dpath, fname))                
+
+    if model.model_name == "rkt":
+        testauc, testacc = evaluate(model, test_loader, model_name, rel, save_test_path)
+    else:
+        testauc, testacc = evaluate(model, test_loader, model_name, save_test_path)
+    print(f"testauc: {testauc}, testacc: {testacc}")
+
+    window_testauc, window_testacc = -1, -1
+    save_test_window_path = os.path.join(save_dir, model.emb_type+"_test_window_predictions.txt")
+    if model.model_name == "rkt":
+        window_testauc, window_testacc = evaluate(model, test_window_loader, model_name, rel, save_test_window_path)
+    else:
+        window_testauc, window_testacc = evaluate(model, test_window_loader, model_name, save_test_window_path)
+    print(f"testauc: {testauc}, testacc: {testacc}, window_testauc: {window_testauc}, window_testacc: {window_testacc}")
+
+    # question_testauc, question_testacc = -1, -1
+    # question_window_testauc, question_window_testacc = -1, -1
+  
+    dres = {
+        "testauc": testauc, "testacc": testacc, "window_testauc": window_testauc, "window_testacc": window_testacc,
+    }  
+
+    q_testaucs, q_testaccs = -1,-1
+    qw_testaucs, qw_testaccs = -1,-1
+    if "test_question_file" in data_config and not test_question_loader is None:
+        save_test_question_path = os.path.join(save_dir, model.emb_type+"_test_question_predictions.txt")
+        q_testaucs, q_testaccs = evaluate_question(model, test_question_loader, model_name, fusion_type, save_test_question_path)
+        for key in q_testaucs:
+            dres["oriauc"+key] = q_testaucs[key]
+        for key in q_testaccs:
+            dres["oriacc"+key] = q_testaccs[key]
+            
+    if "test_question_window_file" in data_config and not test_question_window_loader is None:
+        save_test_question_window_path = os.path.join(save_dir, model.emb_type+"_test_question_window_predictions.txt")
+        qw_testaucs, qw_testaccs = evaluate_question(model, test_question_window_loader, model_name, fusion_type, save_test_question_window_path)
+        for key in qw_testaucs:
+            dres["windowauc"+key] = qw_testaucs[key]
+        for key in qw_testaccs:
+            dres["windowacc"+key] = qw_testaccs[key]
+
+        
+    # print(f"testauc: {testauc}, testacc: {testacc}, window_testauc: {window_testauc}, window_testacc: {window_testacc}")
+    # print(f"question_testauc: {question_testauc}, question_testacc: {question_testacc}, question_window_testauc: {question_window_testauc}, question_window_testacc: {question_window_testacc}")
+    
+    print(dres)
+    raw_config = json.load(open(os.path.join(save_dir,"config.json")))
+    dres.update(raw_config['params'])
+
+    if params['use_wandb'] ==1:
+        wandb.log(dres)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bz", type=int, default=256)
+    parser.add_argument("--save_dir", type=str, default="saved_model")
+    parser.add_argument("--fusion_type", type=str, default="early_fusion,late_fusion")
+    parser.add_argument("--use_wandb", type=int, default=1)
+
+    args = parser.parse_args()
+    print(args)
+    params = vars(args)
+    main(params)
---
+++ pykt-toolkit/pykt/models/rakt.py
@@ -0,0 +1,207 @@
+import copy
+import math
+import numpy as np
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .que_base_model import QueBaseModel,QueEmb
+from ..utils.utils import debug_print
+torch.set_printoptions(precision=4, sci_mode=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def future_mask(seq_length):
+    future_mask = np.triu(np.ones((1, seq_length, seq_length)), k=0).astype('bool') 
+    return torch.from_numpy(future_mask)
+
+def clone(module, num):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(num)])
+
+
+def relative_attention(query, key, value, pos_key_embeds, pos_value_embeds, mask, dropout=None):
+    """Compute scaled dot product attention with relative position embeddings.
+    (https://arxiv.org/pdf/1803.02155.pdf)
+    """
+    assert pos_key_embeds.num_embeddings == pos_value_embeds.num_embeddings
+    
+    scores = torch.matmul(query, key.transpose(-2, -1))     # BS, head, seqlen, seqlen
+    bs, head, seqlen = scores.size(0), scores.size(1), scores.size(2)
+    
+    idxs = torch.arange(scores.size(-1)).to(device)
+    idxs = idxs.view(-1, 1) - idxs.view(1, -1)
+    idxs = torch.clamp(idxs, 0, pos_key_embeds.num_embeddings - 1)
+
+    pos_key = pos_key_embeds(idxs)
+    pos_scores = torch.matmul(query.unsqueeze(-2), pos_key.transpose(-2, -1))
+    scores = scores.unsqueeze(-2) + pos_scores
+    scores = scores / math.sqrt(query.size(-1))
+
+    pos_value = pos_value_embeds(idxs)
+    value = value.unsqueeze(-3) + pos_value
+
+    if mask is not None:
+        scores = scores.masked_fill(mask.unsqueeze(-2), -1e9)
+    prob_attn = F.softmax(scores, dim=-1)
+    pad_zero = torch.zeros(bs, head, 1, 1, seqlen).to(device)
+    prob_attn = torch.cat([pad_zero, prob_attn[:, :, 1:, :, :]], dim=2)
+    # print(prob_attn[2])
+    # sys.exit()
+    if dropout is not None:
+        prob_attn = dropout(prob_attn)
+
+    output = torch.matmul(prob_attn, value).unsqueeze(-2)
+    prob_attn = prob_attn.unsqueeze(-2)
+    
+    return output, prob_attn
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, total_size, num_heads, drop_prob):
+        super(MultiHeadedAttention, self).__init__()
+        assert total_size % num_heads == 0
+        self.total_size = total_size
+        self.head_size = total_size // num_heads
+        self.num_heads = num_heads
+        self.linear_layers = clone(nn.Linear(total_size, total_size), 3)
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, query, key, value, pos_key_embeds, pos_value_embeds, mask=None):
+        batch_size, seq_length = query.shape[:2]
+
+        # Apply mask to all heads
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+
+        # Project inputs
+        query, key, value = [l(x).view(batch_size, seq_length, self.num_heads, self.head_size).transpose(1, 2)
+                             for l, x in zip(self.linear_layers, (query, key, value))]
+
+        # Apply attention
+        out, self.prob_attn = relative_attention(query, key, value, pos_key_embeds, pos_value_embeds, mask, self.dropout)
+        out = out.transpose(1, 2).contiguous().view(batch_size, seq_length, self.total_size)
+        return out, self.prob_attn
+
+
+class RAKT(nn.Module):
+    def __init__(self, num_c, num_q, embed_size, num_attn_layers, num_heads, batch_size, q_matrix,
+                  max_pos, grad_clip, beta=0.5, drop_prob=0.1, emb_type="qid", emb_path=""):
+        """
+        Arguments:
+            num_q (int): number of questions
+            num_c (int): number of skills
+            embed_size (int): input embedding and attention dot-product dimension
+            num_attn_layers (int): number of attention layers
+            num_heads (int): number of parallel attention heads
+            max_pos (int): number of position embeddings to use
+            drop_prob (float): dropout probability
+        """
+        super(RAKT, self).__init__()
+        self.model_name = "rakt"
+        self.emb_type = emb_type
+        self.num_c = num_c
+        self.num_q = num_q
+        self.embed_size = embed_size
+        self.drop_prob = drop_prob
+        self.grad_clip = grad_clip
+        self.beta = beta
+        self.q_matrix = q_matrix
+        
+        print("RAKT change QKT" + "\n")
+        
+        if num_q <= 0:
+            self.que_embeds = nn.Embedding(num_c + 1, embed_size , padding_idx=0)
+        else:
+            self.que_embeds = nn.Embedding(num_q + 1, embed_size , padding_idx=0)
+        self.concept_embeds = nn.Parameter(torch.randn(num_c, embed_size).to(device), requires_grad=True)   #concept embeding
+
+        self.pos_key_embeds = nn.Embedding(max_pos, embed_size // num_heads)
+        self.pos_value_embeds = nn.Embedding(max_pos, embed_size // num_heads)
+
+        self.qc_linear = nn.Linear(2*embed_size, embed_size)
+        self.lin_in = nn.Linear(2*embed_size, embed_size)
+        self.attn_layers = clone(MultiHeadedAttention(embed_size, num_heads, drop_prob), num_attn_layers)
+        self.dropout = nn.Dropout(p=drop_prob)
+        #self.lin_out = nn.Linear(embed_size, 1)
+        self.activate = nn.Sigmoid()
+
+        self.out = nn.Sequential(
+            nn.Linear(embed_size + embed_size, embed_size), 
+            nn.ReLU(), 
+            nn.Dropout(self.drop_prob),
+            nn.Linear(embed_size, embed_size), 
+            nn.ReLU(), 
+            nn.Dropout(self.drop_prob),
+            nn.Linear(embed_size, 1)
+        )
+
+    def get_inter_emb(self, que_inputs, label_inputs):
+        
+        label_inputs = label_inputs.unsqueeze(-1).float()
+
+        inter_emb = torch.cat([que_inputs, que_inputs], dim=-1)
+        inter_emb[..., :self.embed_size] *= label_inputs  
+        inter_emb[..., self.embed_size:] *= 1 - label_inputs  
+        return inter_emb
+    
+    def get_avg_skill_emb(self,c):
+        # add zero for padding
+        concept_emb_cat = torch.cat(
+            [torch.zeros(1, self.embed_size).to(device), self.concept_embeds], dim=0)
+        # shift c
+        related_concepts = (c+1).long()
+        #[batch_size, seq_len, emb_dim]
+        concept_emb_sum = concept_emb_cat[related_concepts, :].sum(axis=-2)
+
+        #[batch_size, seq_len,1]
+        concept_num = torch.where(related_concepts != 0, 1, 0).sum(axis=-1).unsqueeze(-1)
+        concept_num = torch.where(concept_num == 0, 1, concept_num)
+        concept_avg = (concept_emb_sum / concept_num)
+        return concept_avg
+    
+    def qc_relation(self, qemb, all_cemb):
+        sim = torch.matmul(qemb, all_cemb.t())
+        return sim
+
+    def forward(self, dcur, rel_dict, train=True):
+        q, c, r, t = dcur["qseqs"], dcur["cseqs"], dcur["rseqs"], dcur["tseqs"]  
+        qshft, cshft, rshft, tshft = dcur["shft_qseqs"], dcur["shft_cseqs"], dcur["shft_rseqs"], dcur["shft_tseqs"]
+        pid_data = torch.cat((q[:,0:1], qshft), dim=1)
+        c_data = torch.cat((c[:,0:1], cshft), dim=1)
+        target = torch.cat((r[:,0:1], rshft), dim=1)
+        timestamp = torch.cat((t[:,0:1], tshft), dim=1)
+        #print(f"times:{t}, \n timestamp:{timestamp} \n {t.shape}")
+        
+        # filter the dataset only with question, no concept
+        if self.num_q <= 0:
+            inputs = c_data
+        else:
+            inputs = pid_data
+
+        qemb = self.que_embeds(inputs)
+        cemb = self.get_avg_skill_emb(c_data)
+        emb_qc = torch.cat([qemb, cemb], dim=-1)
+        query = self.qc_linear(emb_qc)
+
+        inter_emb = self.get_inter_emb(query, target)
+        inter_emb = F.relu(self.lin_in(inter_emb))
+
+        # calculate qc similarity
+        sim = self.qc_relation(qemb, self.concept_embeds)
+        sim = self.activate(sim)
+        #batch_size, seq_len = inputs.shape[0], inputs.shape[1]
+        #time = computeTime(timestamp, self.time_span, batch_size, seq_len) 
+        
+        mask = future_mask(inter_emb.size(-2)).to(device)
+        outputs, attn  = self.attn_layers[0](query, query, inter_emb, self.pos_key_embeds, self.pos_value_embeds, mask)
+        outputs = self.dropout(outputs)
+        
+        for l in self.attn_layers[1:]:
+            residual, attn = l(outputs, outputs, inter_emb, self.pos_key_embeds,self.pos_value_embeds, mask)
+            outputs = self.dropout(outputs + F.relu(residual))
+        
+        concat_q = torch.cat([outputs, query], dim=-1)
+        out = self.out(concat_q).squeeze(-1)
+        pred = self.activate(out)
+        
+        return pred, sim
